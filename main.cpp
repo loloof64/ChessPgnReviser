d189608e0ed20fe695024fad2b2ae522064f8a3e
Laurent Bernabe <laurent.bernabe@gmail.com>
2020-04-14 15:28:10 +0200

    QMessageBox dialogs buttons are translated with the user locale

--- main.cpp
+++ main.cpp
@@ -3,9 +3,8 @@
 #include <QApplication>
 #include <QTranslator>
 #include <QString>
+#include <QLibraryInfo>
 #include <QDir>
-
-#include <QMessageLogger>
 
 int main(int argc, char *argv[])
 {
@@ -16,8 +15,19 @@ int main(int argc, char *argv[])
     translationsDir.cd("translations");
     const auto path = translationsDir.absolutePath();
 
-    auto success = myappTranslator.load(QLocale(), QString("ChessPgnReviser"), ".", QString(path));
+    myappTranslator.load(QLocale(), QString("ChessPgnReviser"), ".", QString(path));
     app.installTranslator(&myappTranslator);
+
+    QTranslator qtTranslator;
+    qtTranslator.load(QLocale::system(),
+                    "qt", "_",
+                    QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+    app.installTranslator(&qtTranslator);
+
+    QTranslator qtBaseTranslator;
+    qtBaseTranslator.load("qtbase_" + QLocale::system().name(),
+                    QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+    app.installTranslator(&qtBaseTranslator);
 
     MainWindow w;
     w.show();
